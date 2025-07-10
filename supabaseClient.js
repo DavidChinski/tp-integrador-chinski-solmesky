5f4d7f54a560fcecd0b72c2c058af75cca3913bf
Android Studio <devandroid@ort>
2025-07-10 11:46:43 -0300

    back

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,3 +6,5 @@ const supabaseUrl = 'https://umcutcrayjhmfstgqcqq.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtY3V0Y3JheWpobWZzdGdxY3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTY4ODMsImV4cCI6MjA2NzQ3Mjg4M30.hIbfhb36D1zCULnuewVbB-FYNF8DIjvJmH6arwUl4uA';
 
 exports.supabase = createClient(supabaseUrl, supabaseKey);
+
+let a;
